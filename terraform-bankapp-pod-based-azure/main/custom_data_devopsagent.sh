c7d8b0d168c128689044e7ae10a242d7c7683b9f
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-10-09 14:55:13 +0530

    Update custom_data_devopsagent.sh

--- terraform-bankapp-pod-based-azure/main/custom_data_devopsagent.sh
+++ terraform-bankapp-pod-based-azure/main/custom_data_devopsagent.sh
@@ -31,7 +31,7 @@ rm -f node-v16.0.0-linux-x64.tar.gz
 mv /opt/node-v16.0.0-linux-x64 /opt/node-v16.0.0
 cd /opt && curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.38.3
 echo JAVA_HOME="/usr/lib/jvm/jdk-17-oracle-x64" >> /home/demo/.bashrc
-echo PATH="$PATH:$JAVA_HOME/bin:/opt/sonar-scanner/bin:/opt/apache-maven/bin:/opt/node-v16.0.0/bin:/opt/dependency-check/bin:/usr/local/bin" >> /home/demo/.bashrc
+echo PATH="$PATH:$JAVA_HOME/bin:/opt/apache-maven/bin:/opt/node-v16.0.0/bin:/opt/dependency-check/bin:/usr/local/bin" >> /home/demo/.bashrc
 echo "demo  ALL=(ALL)  NOPASSWD:ALL" >> /etc/sudoers 
 yum install -y https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
 yum install -y azure-cli
